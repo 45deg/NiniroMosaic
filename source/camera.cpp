334cc536282260e5dee2d95f7f5ec449fb288c92
45deg <45deg@example.com>
2015-09-28 22:52:28 +0900

    Removed camera

--- source/camera.cpp
+++ source/camera.cpp
@@ -5,30 +5,6 @@
 #include "image_manager.hpp"
 #include "opencv2/opencv.hpp"
 
-cv::Mat createGammaLUT(double gamma){
-    double inverse_gamma = 1.0 / gamma;
-
-    cv::Mat lut_matrix(1, 256, CV_8UC1 );
-    unsigned char* ptr = lut_matrix.ptr();
-    for( int i = 0; i < 256; i++ )
-        ptr[i] = (int)( pow( (double) i / 255.0, inverse_gamma ) * 255.0 );
-
-    return lut_matrix;
-}
-
-
-void changeHSV(cv::Mat& image, double hFactor, double sFactor, double vFactor){
-    cv::Mat hsvImage;
-    cv::cvtColor(image, hsvImage, CV_BGR2HSV);
-    std::vector<cv::Mat> planes;
-    cv::split(hsvImage, planes);
-    planes[0] *= hFactor;
-    planes[1] *= sFactor;
-    planes[2] *= vFactor;
-    cv::merge(planes, hsvImage);
-    cv::cvtColor(hsvImage, image, CV_HSV2BGR);
-}
-
 void equalizeHistgram(cv::Mat& image){
     std::vector<cv::Mat> planes;
     cv::Mat imgHistEqualized;
@@ -63,16 +39,7 @@ int main(int argc, char const* argv[])
         std::cout << std::fixed << "fps: " << fps << std::endl;
 
         cap >> frame;
-/*
-        cv::resize(frame, masterImage, masterImage.size());
-        cv::imshow("master2", masterImage);
-*/
-
         equalizeHistgram(frame);
-        // changeHSV(frame, hf, sf, vf);
-        // cv::LUT(frame, gammaLUT, frameRefined);
-        //frame = cv::imread("/Users/kazuhiro/Pictures/screenshots/a.png");
-        // frame = cv::imread("/Users/kazuhiro/sandbox/4646fa4bc5109.jpg");
 
         cv::resize(frame, masterImage, masterImage.size());
 
@@ -82,7 +49,6 @@ int main(int argc, char const* argv[])
                 cv::Mat cropped;
                 croppedTmp.convertTo(cropped, CV_8UC3);
                 cv::Mat& nearestChip = imageCollections.findNearest(cropped);
-                // outputImage(cv::Rect(j*tileSize, i*tileSize, tileSize, tileSize)) = nearestChip;
                 nearestChip.copyTo(outputImage(cv::Rect(j*tileSize, i*tileSize, tileSize, tileSize)));
             }
         }
