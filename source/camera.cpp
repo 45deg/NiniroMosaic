a503055f0d302394ce5ca871627c15e1342a3e17
45deg <45deg@example.com>
2015-10-04 12:52:30 +0900

    Disable flip when playing movie

--- source/camera.cpp
+++ source/camera.cpp
@@ -12,82 +12,88 @@
 #include <GLFW/glfw3.h>
 #include "glext.h"
 
-void initParser(cmdline::parser& parser){
-    parser.add<int>("height", 'h', "Number of rows of tiles", 100);
-    parser.add<int>("width", 'w', "Number of columns of tiles", 60);
-    parser.add<int>("size", 's', "Image size of tile", 24);
-    parser.add<int>("precision", 'p', "Precision of tile", 5);
-    parser.add<std::string>("directory", 'd', "Directory path of imagesets", "image");
-    parser.add<std::string>("movie", 'm', "path of movie", false);
-}
-
-GLFWwindow* initOpenGLWindow(const cv::Size size, const std::string title){
-    // Initializes the GLFW library.
-    glfwInit();
-    // Create Window
-    GLFWwindow* window = glfwCreateWindow( size.width, size.height, title.c_str(), nullptr, nullptr );
-    glfwMakeContextCurrent( window );
-
-    GLuint texture;
-
-    glGenTextures(1, &texture);
-    glBindTexture(GL_TEXTURE_2D, texture);
-    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_LINEAR);
-
-    return window;
-}
-
-int main(int argc, char * argv[])
-{
-    cmdline::parser args;
-    initParser(args);
-    args.parse_check(argc, argv);
-
-    std::cout << "Set up camera ..." << std::endl;
-
-    cv::VideoCapture cap;
-
-    if(args.exist("movie")) {
-        cap.open(args.get<std::string>("movie"));
-    } else {
-        cap.open(0);
+    void initParser(cmdline::parser& parser){
+        parser.add<int>("height", 'h', "Number of rows of tiles", 100);
+        parser.add<int>("width", 'w', "Number of columns of tiles", 60);
+        parser.add<int>("size", 's', "Image size of tile", 24);
+        parser.add<int>("precision", 'p', "Precision of tile", 5);
+        parser.add<std::string>("directory", 'd', "Directory path of imagesets", "image");
+        parser.add<std::string>("movie", 'm', "path of movie", false);
     }
 
-    cv::Mat frame;
-    int widthTile = args.get<int>("width");
-    int heightTile = args.get<int>("height");
-    int tileSize = args.get<int>("size");
-    int precision = args.get<int>("precision");
+    GLFWwindow* initOpenGLWindow(const cv::Size size, const std::string title){
+        // Initializes the GLFW library.
+        glfwInit();
+        // Create Window
+        GLFWwindow* window = glfwCreateWindow( size.width, size.height, title.c_str(), nullptr, nullptr );
+        glfwMakeContextCurrent( window );
 
-    const cv::Size masterSize = cv::Size(widthTile * precision, heightTile * precision);
+        GLuint texture;
 
-    cv::Mat resizedFrame(masterSize, CV_8UC3);
-    cv::Mat masterImage(masterSize, CV_32FC3);
-    cv::Mat outputImage(cv::Size(widthTile * tileSize, heightTile * tileSize), CV_8UC3);
-    ImageCollections imageCollections(args.get<std::string>("directory"), tileSize, precision); 
-    // run image conversion server
-    std::thread server(Server(8080, tileSize), std::ref(imageCollections));
-    server.detach();
+        glGenTextures(1, &texture);
+        glBindTexture(GL_TEXTURE_2D, texture);
+        glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_LINEAR);
 
-    GLFWwindow* window = initOpenGLWindow(outputImage.size(), "Mosaic");
+        return window;
+    }
 
-    auto prevTime = std::chrono::high_resolution_clock::now();
-
-    // do until window close
-    while(! glfwWindowShouldClose( window ) && cap.isOpened() )
+    int main(int argc, char * argv[])
     {
-        // calc FPS
-        auto curTime = std::chrono::high_resolution_clock::now(); 
-        double fps = 1000./std::chrono::duration_cast<std::chrono::milliseconds>(curTime - prevTime).count();
-        prevTime = curTime;
-        std::cout << std::fixed << "fps: " << fps;
+        cmdline::parser args;
+        initParser(args);
+        args.parse_check(argc, argv);
 
-        // Prosessing image From camera
-        cap >> frame;
 
-        cv::resize(frame, resizedFrame, masterSize);
-        cv::flip(resizedFrame, resizedFrame, 1);
+        int widthTile = args.get<int>("width");
+        int heightTile = args.get<int>("height");
+        int tileSize = args.get<int>("size");
+        int precision = args.get<int>("precision");
+        bool isMovie = args.exist("movie");
+
+        const cv::Size masterSize = cv::Size(widthTile * precision, heightTile * precision);
+
+        cv::Mat resizedFrame(masterSize, CV_8UC3);
+        cv::Mat masterImage(masterSize, CV_32FC3);
+        cv::Mat outputImage(cv::Size(widthTile * tileSize, heightTile * tileSize), CV_8UC3);
+        ImageCollections imageCollections(args.get<std::string>("directory"), tileSize, precision); 
+
+        // run image conversion server
+        std::thread server(Server(8080, tileSize), std::ref(imageCollections));
+        server.detach();
+
+        GLFWwindow* window = initOpenGLWindow(outputImage.size(), "Mosaic");
+
+        // Set up camera
+        std::cout << "Set up camera ..." << std::endl;
+        cv::VideoCapture cap;
+        cv::Mat frame;
+
+        if(isMovie) {
+            cap.open(args.get<std::string>("movie"));
+        } else {
+            cap.open(0);
+        }
+
+        auto prevTime = std::chrono::high_resolution_clock::now();
+
+        // do until window close
+        while(! glfwWindowShouldClose( window ) && cap.isOpened() )
+        {
+            // calc FPS
+            auto curTime = std::chrono::high_resolution_clock::now(); 
+            double fps = 1000./std::chrono::duration_cast<std::chrono::milliseconds>(curTime - prevTime).count();
+            prevTime = curTime;
+            std::cout << std::fixed << "fps: " << fps;
+
+            // Prosessing image From camera
+            cap >> frame;
+
+            cv::resize(frame, resizedFrame, masterSize);
+
+            if(!isMovie)
+            cv::flip(resizedFrame, resizedFrame, 1);
+
         resizedFrame.convertTo(masterImage, CV_32FC3, 1./256);
         cv::cvtColor(masterImage, masterImage, CV_BGR2Lab);
 
